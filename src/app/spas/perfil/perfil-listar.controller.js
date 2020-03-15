5521a90e76a672e1edcc7eb3b1b2664359c27cb0
Tom <tom.robson@hotmail.com>
2020-03-15 01:18:11 -0300

    atualizando controller e template de perfil

--- src/app/spas/perfil/perfil-listar.controller.js
+++ src/app/spas/perfil/perfil-listar.controller.js
@@ -3,44 +3,46 @@ PerfilListarController.$inject = ["$rootScope", "$scope", "$location",
     "$q", '$filter', '$routeParams', 'HackatonStefaniniService'];
 
 function PerfilListarController($rootScope, $scope, $location,
-    $q, $filter, $routeParams, HackatonStefaniniService){
-    
+    $q, $filter, $routeParams, HackatonStefaniniService) {
+    vm = this;
+
     vm.qdePorPagina = 5;
     vm.ultimoIndex = 0;
     vm.contador = 0;
 
-    vm.url = "http://localhost:8080/treinamento/api/pessoas/";
-    vm.urlPerfil = "http://localhost:8080/treinamento/api/perfils/";
+    vm.url = "http://localhost:8080/treinamento/api/perfils/";
+    vm.urlPessoa = "http://localhost:8080/treinamento/api/pessoas/";
 
     vm.init = function () {
-        HackatonStefaniniService.listar(vm.urlPerfil).then(
+        HackatonStefaniniService.listar(vm.url).then(
             function (responsePerfil) {
-                if (responsePerfil.data !== undefined)
-                    vm.listaPerfil = responsePerfil.data;
+                if (responsePerfil.data !== undefined) {
+                    vm.listarPerfis = responsePerfil.data;
 
-                vm.listaPerfilMostrar = [];
-                var max = vm.listaPerfil.length > vm.qdePorPagina ? vm.qdePorPagina : vm.listaPerfil.length;
+                    vm.listaPerfilMostar = [];
+                    var max = vm.listarPerfis.length > vm.qdePorPagina ? vm.qdePorPagina : vm.listarPerfis.length;
 
-                vm.qdePaginacao = new Array(vm.listaPerfil.length % vm.qdePorPagina === 0 ? vm.listaPerfil.length / vm.qdePorPagina : parseInt(vm.listaPerfil.length / vm.qdePorPagina) + 1);
-                vm.currentPage = 1;
-                for (var count = 0; count < max; count++) {
-                    vm.listaPerfilMostrar.push(vm.listaPerfil[count]);
-                    vm.ultimoIndex++;
-                }
-
-                vm.listaPerfilMostrar.sort(function (a, b) {
-                    return a.id - b.id;
-                });
-
-                HackatonStefaniniService.listar(vm.urlPerfil).then(
-                    function (responsePerfil) {
-                        if (responsePerfil.data !== undefined)
-                            vm.listaPerfil = responsePerfil.data;
+                    vm.qdePorPagina = new Array(vm.listarPerfis.length % vm.qdePorPagina === 0 ? vm.listarPerfis.length / vm.qdePorPagina : parseInt(vm.listarPerfis.length / vm.qdePorPagina) + 1);
+                    vm.currentPage = 1;
+                    for (var count = 0; count < max; count++) {
+                        vm.listaPerfilMostar.push(vm.listarPerfis[count]);
+                        vm.ultimoIndex++;
                     }
-                );
+
+                    vm.listaPerfilMostar.sort(function (a, b) {
+                        return a.id - b.id;
+                    });
+
+                    HackatonStefaniniService.listar(vm.urlPessoa).then(
+                        function (responsePessoa) {
+                            if (responsePessoa.data !== undefined)
+                                vm.listarPessoa = responsePessoa.data;
+                        }
+                    )
+                }
             }
-        );
-    };
+        )
+    }
 
     vm.atualizarPaginanacao = function (index) {
 
@@ -51,21 +53,21 @@ function PerfilListarController($rootScope, $scope, $location,
     };
 
     vm.avancarPaginanacao = function (index) {
-        
-        vm.listaPerfilMostrar = [];
+        vm.listaPerfilMostar = [];
         vm.currentPage++;
 
         var idx = angular.copy(vm.ultimoIndex);
-        var cont = vm.listaPerfil.length - vm.qdePorPagina;
+        var cont = vm.listarPerfis.length - vm.qdePorPagina;
         for (var count = cont > vm.qdePorPagina ? vm.qdePorPagina : cont; count > 0; count--) {
-            vm.listaPerfilMostrar.push(vm.listaPerfil[idx++]);
+            vm.listaPerfilMostar.push(vm.listarPerfis[idx++]);
             vm.ultimoIndex++;
             vm.contador++;
         }
-        vm.listaPerfilMostrar.sort(function (a, b) {
+
+        vm.listaPerfilMostar.sort(function (a, b) {
             return a.id - b.id;
         });
-    };
+    }
 
     vm.retrocederPaginanacao = function (index) {
         
@@ -75,42 +77,41 @@ function PerfilListarController($rootScope, $scope, $location,
         var idx = vm.contador - 1;
         vm.ultimoIndex = idx + 1;
         for (var count = vm.qdePorPagina; count > 0; count--) {
-            vm.listaPerfilMostrar.push(vm.listaPerfil[idx--]);
+            vm.listaPerfilMostrar.push(vm.listarPerfis[idx--]);
             vm.contador--;
         }
-        vm.listaPerfilMostrar.sort(function (a, b) {
+        vm.listaPessoasMostrar.sort(function (a, b) {
             return a.id - b.id;
         });
     };
 
     vm.editar = function (id) {
         if (id !== undefined)
-            $location.path("EditarPerfil/" + id);
+            $location.path("EditarPerfis/" + id);
         else
-            $location.path("cadastrarPerfil");
+            $location.path("cadastrarPerfis");
     }
 
     vm.remover = function (id) {
-
         var liberaExclusao = true;
 
-        angular.forEach(vm.listaPerfil, function (value, key) {
+        angular.forEach(vm.listarPerfis, function (value, key) {
             if (value.id === id)
                 liberaExclusao = false;
         });
 
         if (liberaExclusao)
-            HackatonStefaniniService.excluir(vm.urlPerfil + id).then(
+            HackatonStefaniniService.excluir(vm.url + id).then(
                 function (response) {
                     vm.init();
                 }
             );
         else {
-            alert("Pessoa com Endereço vinculado, exclusão não permitida");
+            alert("Perfil com Pessoa vinculado, exclusão não permitida");
         }
     }
 
     vm.retornarTelaListagem = function () {
-        $location.path("listarPerfil");
+        $location.path("listarPerfis");
     }
 }
