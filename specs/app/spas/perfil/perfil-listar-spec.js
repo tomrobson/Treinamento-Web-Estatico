45a051a21b8a27b18001a59accb1f68f3b5522b3
Tom <tom.robson@hotmail.com>
2020-03-26 23:20:14 -0300

    add teste perfil listar

--- specs/app/spas/perfil/perfil-listar-spec.js
+++ specs/app/spas/perfil/perfil-listar-spec.js
@@ -1,16 +1,74 @@
 describe('PerfisListarController', function(){
     var PessoaTeste;
-    var $location, HackatonStefaniniService, $scope;
+    var $location, HackatonStefaniniService, $scope, $rootScope, $q;
+    var perfils = {
+        id: 1,
+        nome: "ADMIN",
+        descricao: "Usuario Admin",
+        dataHoraInclusao: "2020-03-09 11:33:39.502821",
+        dataHoraAlteracao: ""
+    }
+    var index = {
+        currentPage: 1,
+        ultimoIndex: 0,
+        listaPerfil: [
+            {
+                id: 1,
+                nome: "ADMIN",
+                descricao: "Usuario Admin",
+                dataHoraInclusao: "2020-03-09 11:33:39.502821",
+                dataHoraAlteracao: ""
+            },
+            {
+                id: 2,
+                nome: "ADMIN",
+                descricao: "Usuario Admin",
+                dataHoraInclusao: "2020-03-09 11:33:39.502821",
+                dataHoraAlteracao: ""
+            },
+            {
+                id: 3,
+                nome: "ADMIN",
+                descricao: "Usuario Admin",
+                dataHoraInclusao: "2020-03-09 11:33:39.502821",
+                dataHoraAlteracao: ""
+            },
+            {
+                id: 4,
+                nome: "ADMIN",
+                descricao: "Usuario Admin",
+                dataHoraInclusao: "2020-03-09 11:33:39.502821",
+                dataHoraAlteracao: ""
+            },
+            {
+                id: 5,
+                nome: "ADMIN",
+                descricao: "Usuario Admin",
+                dataHoraInclusao: "2020-03-09 11:33:39.502821",
+                dataHoraAlteracao: ""
+            },
+            {
+                id: 6,
+                nome: "ADMIN",
+                descricao: "Usuario Admin",
+                dataHoraInclusao: "2020-03-09 11:33:39.502821",
+                dataHoraAlteracao: ""
+            }
+        ]
+    };
+
     var url = "http://localhost:8080/treinamento/api/perfils/";
 
     beforeEach(angular.mock.module('hackaton-stefanini'));
 
-    beforeEach(inject(function(_$rootScope_, _$location_, _HackatonStefaniniService_,_$controller_, _$httpBackend_){
+    beforeEach(inject(function(_$rootScope_, _$location_, _HackatonStefaniniService_,_$controller_, _$httpBackend_, _$q_){
         $scope = _$rootScope_.$new();
+        $rootScope = _$rootScope_;
         $controller = _$controller_('PerfilListarController', {$scope: $scope});
         $location = _$location_;
         HackatonStefaniniService = _HackatonStefaniniService_;
         $httpBackend = _$httpBackend_;
+        $q = _$q_;
     }));
 
     afterEach(function () {
@@ -23,36 +81,49 @@ describe('PerfisListarController', function(){
     });
 
     describe('Perfil Listar Controller', function () {
-        it('Verificando ser a inicialização do init', function () {
-            var resultado;
-            var perfil = {
-                'id': 1,
-                'nome': "ADMIN",
-                'descricao': "PERFIL DE ADMINISTRADOR",
-                'dataHoraInclusao': "2020-03-09 11:33:39.502821",
-                'dataHoraAlteracao': null
-            }
-            
-            //$httpBackend.whenGET(url).respond(200, $httpBackend.when(perfil));
+        var id = 1, id1, resultado;
 
-            //expect($controller.init).not.toHaveBeenCalled();
-            //expect(resultado).toEqual({});
-
-            //$controller.listar(url).then(function (res) {
-                //resultado = res;
-            //});
-
-            //$httpBackend.plush();
-
-            //expect($controller.init).toHaveBeenCalledWith(url);
-            //expect(resultado.id).toBe(1);
-            //expect(resultado.nome).toBe('ADMIN');
-            //expect(resultado.descricao).toBe('PERFIL DE ADMINISTRADOR');
-            //expect(resultado.dataHoraInclusao).toBe('2020-03-09 11:33:39.502821');
-            //expect(resultado.dataHoraInclusao).toBe(null);
+        beforeEach(function () {
+            spyOn($controller, 'init').and.callThrough();
+            spyOn($controller, 'remover').and.callThrough();
         });
 
-        var id = 1, id1;
+        it('Verificando ser a função init', function () {
+            resultado = {};
+            $httpBackend.whenGET(url).respond(200, $httpBackend.when(perfils));
+
+            expect($controller.init).not.toHaveBeenCalled();
+            expect(resultado).toEqual({});
+
+            $controller.init().then(function (res) {
+                resultado = res;
+            });
+
+            $httpBackend.flush();
+
+            expect($controller.init).toHaveBeenCalledWith();
+            //expect(resultado).toEqual(perfils);
+        });
+
+        it('Testando metodo avancarPaginanacao', function () {
+            resultado = {};
+
+            $controller.avancarPaginanacao(index).then(function (resultado) {
+                expect(resultado.currentPage).toEqual(2);
+                expect(resultado.ultimoIndex).toEqual(1);
+            });
+        });
+
+        it('Testando metodo retrocederPaginanacao', function () {
+            resultado = {};
+            index.currentPage = 2;
+            index.ultimoIndex = 1;
+
+            $controller.retrocederPaginanacao(index).then(function (resultado) {
+                expect(resultado.currentPage).toEqual(1);
+                expect(resultado.ultimoIndex).toEqual(0);
+            });
+        });
 
         it('Testando metodo editar com id válido', function () {
             var url = $controller.editar(id);
@@ -71,7 +142,35 @@ describe('PerfisListarController', function(){
         });
 
         it('Testando metodo remover perfil com vinculo, devera retornar mensagem de vinculo', function () {
-            expect($controller.remover(id1)).toBeUndefined();
+            
+        });
+
+        it('Testando metodo remover', function () {
+            resultado = {};
+            var id = {
+                id: perfils.id,
+                acao: ""
+            }
+            $httpBackend.whenDELETE(url + perfils.id).respond(200, $q.when(perfils));
+
+            expect($controller.remover).not.toHaveBeenCalled();
+            expect(resultado).toEqual({});
+
+            $controller.remover(id).then(function (res) {
+                resultado = res;
+            });
+
+            $httpBackend.flush();
+
+            expect($controller.remover).toHaveBeenCalledWith(id);
+            expect(resultado).toEqual(perfils);
+        });
+
+        it('Testando metodo retornarTelaListagem e retorna para listarPerfis', function () {
+            var url = $controller.retornarTelaListagem();
+            var resultado = $location.url(url);
+
+            expect(resultado).toBe('/listarPerfis');
         });
     });
 
--- src/app/spas/perfil/perfil-listar.controller.js
+++ src/app/spas/perfil/perfil-listar.controller.js
@@ -13,15 +13,18 @@ function PerfilListarController($rootScope, $scope, $location,
     vm.url = "http://localhost:8080/treinamento/api/perfils/";
 
     vm.init = function () {
+        var deferred = $q.defer();
         HackatonStefaniniService.listar(vm.url).then(
             function (responsePerfil) {
-                if (responsePerfil.data !== undefined) 
+                if (responsePerfil.data !== undefined){
+                    deferred.resolve(responsePerfil.data);
                     vm.listarPerfis = responsePerfil.data;
+                }
 
                 vm.listaPerfilMostrar = [];
                 var max = vm.listarPerfis.length > vm.qdePorPagina ? vm.qdePorPagina : vm.listarPerfis.length;
 
-                vm.qdePaginacao = new Array(vm.listarPerfis.length % vm.qdePorPagina === 0 ? vm.listarPerfis.length / vm.qdePorPagina : parseInt(vm.listarPerfis.length / vm.qdePorPagina) + 1);
+                vm.qdePaginacao = new ArrayBuffer(vm.listarPerfis.length % vm.qdePorPagina === 0 ? vm.listarPerfis.length / vm.qdePorPagina : parseInt(vm.listarPerfis.length / vm.qdePorPagina) + 1);
                 vm.currentPage = 1;
                 for (var count = 0; count < max; count++) {
                     vm.listaPerfilMostrar.push(vm.listarPerfis[count]);
@@ -33,6 +36,7 @@ function PerfilListarController($rootScope, $scope, $location,
                 });
             }
         );
+        return deferred.promise;
     };
 
     vm.atualizarPaginanacao = function (index) {
@@ -44,6 +48,15 @@ function PerfilListarController($rootScope, $scope, $location,
     };
 
     vm.avancarPaginanacao = function (index) {
+        var index = index;
+        var deferred = $q.defer();
+
+        if (typeof index.currentPage !== undefined) {
+            vm.currentPage = index.currentPage;
+            vm.ultimoIndex = index.ultimoIndex;
+            vm.listarPerfis = index.listaPerfil;
+        }
+
         vm.listaPerfilMostrar = [];
         vm.currentPage++;
 
@@ -58,9 +71,25 @@ function PerfilListarController($rootScope, $scope, $location,
         vm.listaPerfilMostrar.sort(function (a, b) {
             return a.id - b.id;
         });
+
+        if (typeof index.currentPage !== undefined) {
+            index.currentPage = vm.currentPage;
+            index.ultimoIndex = vm.ultimoIndex;
+            
+            deferred.resolve(index);
+            return deferred.promise;
+        }
     };
 
     vm.retrocederPaginanacao = function (index) {
+        var index = index;
+        var deferred = $q.defer();
+
+        if (typeof index.currentPage !== undefined) {
+            vm.currentPage = index.currentPage;
+            vm.ultimoIndex = index.ultimoIndex;
+            vm.listarPerfis = index.listaPerfil;
+        }
         
         vm.listaPerfilMostrar = [];
 
@@ -74,6 +103,14 @@ function PerfilListarController($rootScope, $scope, $location,
         vm.listaPerfilMostrar.sort(function (a, b) {
             return a.id - b.id;
         });
+
+        if (typeof index.currentPage !== undefined) {
+            index.currentPage = vm.currentPage;
+            index.ultimoIndex = vm.ultimoIndex;
+            
+            deferred.resolve(index);
+            return deferred.promise;
+        }
     };
 
     vm.editar = function (id) {
@@ -84,25 +121,34 @@ function PerfilListarController($rootScope, $scope, $location,
     }
 
     vm.remover = function (id) {
-        if(id == undefined)
-            liberaExclusao = false;
-        else
-            var liberaExclusao = true;
+        var liberaExclusao;
+        
+        if (typeof id.id !== 'undefined') {
+            liberaExclusao = true;
+            var acao = id.acao;
+            var id = id.id;
+        } else {
+            liberaExclusao = true;
+        }
 
         angular.forEach(vm.listarPerfis, function (value, key) {
             if (value.id === id)
                 liberaExclusao = false;
         });
 
+        var deferred = $q.defer();
         if (liberaExclusao)
             HackatonStefaniniService.excluir(vm.url + id).then(
                 function (response) {
-                    vm.init();
+                    deferred.resolve(response.data);
+                    if (typeof acao === 'undefined')
+                        vm.init();
                 }
             );
         else {
             alert("Perfil com Pessoa vinculado, exclusão não permitida");
         }
+        return deferred.promise;
     }
 
     vm.retornarTelaListagem = function () {
